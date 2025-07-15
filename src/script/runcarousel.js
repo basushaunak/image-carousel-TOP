d4b71d30240c06e8bb60f2c9d7f746e4a6f5dcc7
Shaunak Basu <55021457+basushaunak@users.noreply.github.com>
2025-07-15 14:05:24 +0530

    Completed

--- src/script/runcarousel.js
+++ src/script/runcarousel.js
@@ -3,6 +3,8 @@ function runCarousel(){
     const carouselNav = document.querySelector(".carousel-nav");
     const carouselButtonPrev = document.querySelector(".carousel-button--prev");
     const carouselButtonNext = document.querySelector(".carousel-button--next");
+    const slidesContainer = document.querySelector(".slides-container");
+    let isHoveringSlide;
 
     let activeSlide = 0;
     let targetSlide = -1;
@@ -18,19 +20,48 @@ function runCarousel(){
     slideList[activeSlide].classList.add("slide-active");
 
     carouselButtonPrev.addEventListener("click",()=>{
-        targetSlide = activeSlide - 1;
-        if(targetSlide < 0){
-            targetSlide = slideList.length-1;
+        previousSlide();
+    })
+    carouselButtonNext.addEventListener("click",()=>{
+        nextSlide();
+    })
+
+    carouselNav.addEventListener("click",(e)=>{
+        let clickedButton = e.target.closest("button");
+        for(let i = 0; i< carouselNavButtons.length;i++){
+            if(carouselNavButtons[i] === clickedButton){
+                targetSlide = i;
+                break;
+            }
         }
         updateCarousel();
     })
-    carouselButtonNext.addEventListener("click",()=>{
+
+    slidesContainer.addEventListener("mouseleave",()=>{
+        console.log("Mouse left slide");
+        isHoveringSlide = false;
+    })
+
+    slidesContainer.addEventListener("mouseenter",()=>{
+        console.log("Mouse entered slide");
+        isHoveringSlide = true;
+    })
+
+    function nextSlide(){
         targetSlide = activeSlide + 1;
         if(targetSlide >= slideList.length){
             targetSlide = 0;
         }
         updateCarousel();
-    })
+    }
+
+    function previousSlide(){
+        targetSlide = activeSlide - 1;
+        if(targetSlide < 0){
+            targetSlide = slideList.length-1;
+        }
+        updateCarousel();
+    }
 
     function updateCarousel(){
         carouselNavButtons[activeSlide].classList.remove("slide-active");
@@ -40,5 +71,14 @@ function runCarousel(){
         activeSlide = targetSlide;
         targetSlide = -1;
     }
+
+    function autoLoopSlides(){
+        if(isHoveringSlide){
+            return;
+        }
+        nextSlide();
+    }
+
+    setInterval(autoLoopSlides,5000);
 }
 runCarousel();
