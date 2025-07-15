de84fdab7451884967be77f3e4c81ff60aa4a189
Shaunak Basu <55021457+basushaunak@users.noreply.github.com>
2025-07-15 08:36:34 +0530

    WIP

--- src/script/runcarousel.js
+++ src/script/runcarousel.js
@@ -1,6 +1,9 @@
 function runCarousel(){
     const slideList = document.querySelectorAll(".slide");
     const carouselNav = document.querySelector(".carousel-nav");
+    const carouselButtonPrev = document.querySelector(".carousel-button--prev");
+    const carouselButtonNext = document.querySelector(".carousel-button--next");
+
     let activeSlide = 0;
     let targetSlide = -1;
     let btn;
@@ -13,5 +16,29 @@ function runCarousel(){
     const carouselNavButtons = document.querySelectorAll(".carousel-nav--button");
     carouselNavButtons[activeSlide].classList.add("slide-active");
     slideList[activeSlide].classList.add("slide-active");
+
+    carouselButtonPrev.addEventListener("click",()=>{
+        targetSlide = activeSlide - 1;
+        if(targetSlide < 0){
+            targetSlide = slideList.length-1;
+        }
+        updateCarousel();
+    })
+    carouselButtonNext.addEventListener("click",()=>{
+        targetSlide = activeSlide + 1;
+        if(targetSlide >= slideList.length){
+            targetSlide = 0;
+        }
+        updateCarousel();
+    })
+
+    function updateCarousel(){
+        carouselNavButtons[activeSlide].classList.remove("slide-active");
+        slideList[activeSlide].classList.remove("slide-active");
+        carouselNavButtons[targetSlide].classList.add("slide-active");
+        slideList[targetSlide].classList.add("slide-active");
+        activeSlide = targetSlide;
+        targetSlide = -1;
+    }
 }
 runCarousel();
